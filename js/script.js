dd62cd4c2fbda71cde6dcf62441aa21698a28e45
GiulianoCretella <giulianocretella.1@gmail.com>
2022-04-11 16:49:59 +0200

    funzione selezione

--- js/script.js
+++ js/script.js
@@ -123,15 +123,29 @@ const icon = [
 	}
 ];
 // Milestone 1
-// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome 
+// Milestone 2
+// Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.dell'icona e l'icona stessa.
 const container = document.querySelector('.container');
 icon.forEach((element)=>{
    const cards = document.createElement('div');
    cards.setAttribute('class','card');
    cards.innerHTML=`
-   <i class="${element.family} ${element.prefix}${element.name}"></i>
+   <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
    <p>${element.name}</p>
    `;
 
    container.append(cards)
 })
+// Milestone 3
+// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+let selezione = document.getElementById("selezione");
+
+function selezionaIcone(){
+    let indiceOpzione = selezione.selectedIndex;
+    let opzione =selezione.options[indiceOpzione].value;
+    console.log(opzione)
+}
+
+
+selezione.addEventListener('change', selezionaIcone);
