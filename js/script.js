27d912dcb35f18671f91d125e61d0736b343fb06
GiulianoCretella <giulianocretella.1@gmail.com>
2022-04-11 18:22:03 +0200

    colori random e condizione con switch

--- js/script.js
+++ js/script.js
@@ -14,112 +14,112 @@ const icon = [
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'orange'
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'orange'
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'orange'
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'orange'
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'orange'
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'orange'
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'orange'
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'orange'
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'green'
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'green'
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'green'
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'green'
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'blue'
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'blue'
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'blue'
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color:  '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)
+		color: 'blue'
 	}
 ];
 // Milestone 1
@@ -127,16 +127,7 @@ const icon = [
 // Milestone 2
 // Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.dell'icona e l'icona stessa.
 const container = document.querySelector('.container');
-icon.forEach((element)=>{
-	container.innerHTML+='';
-	const cards = document.createElement('div');
-	cards.setAttribute('class','card');
-	cards.innerHTML=`
-	<i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-	<p>${element.name}</p>
-	`;
-	container.append(cards)
- })
+
 // Milestone 3
 // Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
 let selezione = document.getElementById("selezione");
@@ -146,7 +137,21 @@ function selezionaIcone(){
     let indiceOpzione = selezione.selectedIndex;
     let opzione =selezione.options[indiceOpzione].value;
     console.log(opzione)
-    if(opzione === 'animals'){
+	switch(opzione){
+		default :
+			container.innerHTML ='';
+			icon.forEach((element)=>{
+				container.innerHTML+='';
+				const cards = document.createElement('div');
+				cards.setAttribute('class','card');
+				cards.innerHTML=`
+				<i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+				<p>${element.name}</p>
+				`;
+				container.append(cards)
+			 })
+		break;
+		case 'animals':
 		container.innerHTML ='';
         const animals = icon.filter((element)=> element.type ==='animal');
         console.log(animals)
@@ -158,9 +163,10 @@ function selezionaIcone(){
             <p>${element.name}</p>
             `;
             container.append(cards)
-         })
-    }else if(opzione === 'vegetables'){
-        container.innerHTML='';
+	    });
+		break;
+		case 'vegetables':
+	    container.innerHTML='';
         const vegetables = icon.filter((element)=> element.type ==='vegetable');
         console.log(vegetables)
         vegetables.forEach((element)=>{
@@ -171,32 +177,76 @@ function selezionaIcone(){
             <p>${element.name}</p>
             `;
             container.append(cards)
-         })
-    }else if(opzione === 'users'){
-        container.innerHTML+='';
-        const users = icon.filter((element)=> element.type === 'user');
-        console.log(users)
-        users.forEach((element)=>{
-            const cards = document.createElement('div');
-            cards.setAttribute('class','card');
-            cards.innerHTML=`
-            <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-            <p>${element.name}</p>
-            `;
-            container.append(cards)
-         })
-    }else{
-        icon.forEach((element)=>{
-            container.innerHTML+='';
-            const cards = document.createElement('div');
-            cards.setAttribute('class','card');
-            cards.innerHTML=`
-            <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-            <p>${element.name}</p>
-            `;
-            container.append(cards)
-         })
-    }
+        });
+		 break;
+		 case 'users':
+			container.innerHTML+='';
+			const users = icon.filter((element)=> element.type === 'user');
+			console.log(users)
+			users.forEach((element)=>{
+				const cards = document.createElement('div');
+				cards.setAttribute('class','card');
+				cards.innerHTML=`
+				<i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+				<p>${element.name}</p>
+				`;
+				container.append(cards)
+		    })
+			break;
+		}
+
+
+    // if(opzione === 'animals'){
+	// 	container.innerHTML ='';
+    //     const animals = icon.filter((element)=> element.type ==='animal');
+    //     console.log(animals)
+    //     animals.forEach((element)=>{
+    //         const cards = document.createElement('div');
+    //         cards.setAttribute('class','card');
+    //         cards.innerHTML=`
+    //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+    //         <p>${element.name}</p>
+    //         `;
+    //         container.append(cards)
+    //      })
+    // }else if(opzione === 'vegetables'){
+    //     container.innerHTML='';
+    //     const vegetables = icon.filter((element)=> element.type ==='vegetable');
+    //     console.log(vegetables)
+    //     vegetables.forEach((element)=>{
+    //         const cards = document.createElement('div');
+    //         cards.setAttribute('class','card');
+    //         cards.innerHTML=`
+    //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+    //         <p>${element.name}</p>
+    //         `;
+    //         container.append(cards)
+    //      })
+    // }else if(opzione === 'users'){
+    //     container.innerHTML+='';
+    //     const users = icon.filter((element)=> element.type === 'user');
+    //     console.log(users)
+    //     users.forEach((element)=>{
+    //         const cards = document.createElement('div');
+    //         cards.setAttribute('class','card');
+    //         cards.innerHTML=`
+    //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+    //         <p>${element.name}</p>
+    //         `;
+    //         container.append(cards)
+    //      })
+    // }else{
+    //     icon.forEach((element)=>{
+    //         container.innerHTML+='';
+    //         const cards = document.createElement('div');
+    //         cards.setAttribute('class','card');
+    //         cards.innerHTML=`
+    //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+    //         <p>${element.name}</p>
+    //         `;
+    //         container.append(cards)
+    //      })
+    // }
    
 }
 
@@ -207,12 +257,18 @@ selezione.addEventListener('change', selezionaIcone);
 
 // 1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
 
+const getRanHex = size => {
+	let result = [];
+	let hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+  
+	for (let n = 0; n < size; n++) {
+	  result.push(hexRef[Math.floor(Math.random() * 16)]);
+	}
+	return result.join('');
+  }
 
-
-function getRandomInt(min, max){
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max -min) + min);
-}
-const color = '#'+ getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9)+getRandomInt(0,9);
-console.log(color)
+const colori = icon.map((element)=>{
+	return element.color = '#'+ getRanHex(6);
+})
+console.log(colori)
+console.log(icon)
