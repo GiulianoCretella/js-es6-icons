bc17ea526bd714f8130d402a6555b14ea61c6fad
GiulianoCretella <giulianocretella.1@gmail.com>
2022-04-12 10:14:14 +0200

    refactory codice

--- js/script.js
+++ js/script.js
@@ -128,16 +128,20 @@ const icon = [
 // Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.dell'icona e l'icona stessa.
 const container = document.querySelector('.container');
 container.innerHTML ='';
-icon.forEach((element)=>{
-	container.innerHTML+='';
-	const cards = document.createElement('div');
-	cards.setAttribute('class','card');
-	cards.innerHTML=`
-	<i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-	<p>${element.name}</p>
-	`;
-	container.append(cards)
- })
+function printAllCards(icon){
+	icon.forEach((element)=>{
+		container.innerHTML+='';
+		const cards = document.createElement('div');
+		cards.setAttribute('class','card');
+		cards.innerHTML=`
+		<i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+		<p>${element.name}</p>
+		`;
+		container.append(cards)
+	})
+}
+printAllCards(icon)
+
 
 // Milestone 3
 // Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
@@ -145,126 +149,92 @@ let selezione = document.getElementById("selezione");
 
 function selezionaIcone(){
     let indiceOpzione = selezione.selectedIndex;
-    let opzione =selezione.options[indiceOpzione].value;
+    let opzione = selezione.options[indiceOpzione].value;
     console.log(opzione)
 	switch(opzione){
 		case 'all': default:
-				container.innerHTML ='';
-			icon.forEach((element)=>{
-				container.innerHTML+='';
-				const cards = document.createElement('div');
-				cards.setAttribute('class','card');
-				cards.innerHTML=`
-				<i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-				<p>${element.name}</p>
-				`;
-				container.append(cards)
-			 })
+			container.innerHTML='';
+			printAllCards(icon)
 		break;
 		
 		case 'animals':
-		container.innerHTML ='';
-        const animals = icon.filter((element)=> element.type ==='animal');
-        console.log(animals)
-        animals.forEach((element)=>{
-            const cards = document.createElement('div');
-            cards.setAttribute('class','card');
-            cards.innerHTML=`
-            <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-            <p>${element.name}</p>
-            `;
-            container.append(cards)
-	    });
+			container.innerHTML ='';
+			const animals = icon.filter((element)=> element.type ==='animal');
+			console.log(animals)
+			printAllCards(animals)
 		break;
+
 		case 'vegetables':
-	    container.innerHTML='';
-        const vegetables = icon.filter((element)=> element.type ==='vegetable');
-        console.log(vegetables)
-        vegetables.forEach((element)=>{
-            const cards = document.createElement('div');
-            cards.setAttribute('class','card');
-            cards.innerHTML=`
-            <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-            <p>${element.name}</p>
-            `;
-            container.append(cards)
-        });
-		 break;
-		 case 'users':
-			container.innerHTML+='';
+			container.innerHTML='';
+			const vegetables = icon.filter((element)=> element.type ==='vegetable');
+			console.log(vegetables)
+			printAllCards(vegetables)
+		break;
+		case 'users':
+			container.innerHTML='';
 			const users = icon.filter((element)=> element.type === 'user');
 			console.log(users)
-			users.forEach((element)=>{
-				const cards = document.createElement('div');
-				cards.setAttribute('class','card');
-				cards.innerHTML=`
-				<i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-				<p>${element.name}</p>
-				`;
-				container.append(cards)
-		    })
-			break;
+		    printAllCards(users)
+		break;
 		}
 
 
-    // if(opzione === 'animals'){
-	// 	container.innerHTML ='';
-    //     const animals = icon.filter((element)=> element.type ==='animal');
-    //     console.log(animals)
-    //     animals.forEach((element)=>{
-    //         const cards = document.createElement('div');
-    //         cards.setAttribute('class','card');
-    //         cards.innerHTML=`
-    //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-    //         <p>${element.name}</p>
-    //         `;
-    //         container.append(cards)
-    //      })
-    // }else if(opzione === 'vegetables'){
-    //     container.innerHTML='';
-    //     const vegetables = icon.filter((element)=> element.type ==='vegetable');
-    //     console.log(vegetables)
-    //     vegetables.forEach((element)=>{
-    //         const cards = document.createElement('div');
-    //         cards.setAttribute('class','card');
-    //         cards.innerHTML=`
-    //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-    //         <p>${element.name}</p>
-    //         `;
-    //         container.append(cards)
-    //      })
-    // }else if(opzione === 'users'){
-    //     container.innerHTML+='';
-    //     const users = icon.filter((element)=> element.type === 'user');
-    //     console.log(users)
-    //     users.forEach((element)=>{
-    //         const cards = document.createElement('div');
-    //         cards.setAttribute('class','card');
-    //         cards.innerHTML=`
-    //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-    //         <p>${element.name}</p>
-    //         `;
-    //         container.append(cards)
-    //      })
-    // }else{
-    //     icon.forEach((element)=>{
-    //         container.innerHTML+='';
-    //         const cards = document.createElement('div');
-    //         cards.setAttribute('class','card');
-    //         cards.innerHTML=`
-    //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
-    //         <p>${element.name}</p>
-    //         `;
-    //         container.append(cards)
-    //      })
-    // }
+//     // if(opzione === 'animals'){
+// 	// 	container.innerHTML ='';
+//     //     const animals = icon.filter((element)=> element.type ==='animal');
+//     //     console.log(animals)
+//     //     animals.forEach((element)=>{
+//     //         const cards = document.createElement('div');
+//     //         cards.setAttribute('class','card');
+//     //         cards.innerHTML=`
+//     //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+//     //         <p>${element.name}</p>
+//     //         `;
+//     //         container.append(cards)
+//     //      })
+//     // }else if(opzione === 'vegetables'){
+//     //     container.innerHTML='';
+//     //     const vegetables = icon.filter((element)=> element.type ==='vegetable');
+//     //     console.log(vegetables)
+//     //     vegetables.forEach((element)=>{
+//     //         const cards = document.createElement('div');
+//     //         cards.setAttribute('class','card');
+//     //         cards.innerHTML=`
+//     //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+//     //         <p>${element.name}</p>
+//     //         `;
+//     //         container.append(cards)
+//     //      })
+//     // }else if(opzione === 'users'){
+//     //     container.innerHTML+='';
+//     //     const users = icon.filter((element)=> element.type === 'user');
+//     //     console.log(users)
+//     //     users.forEach((element)=>{
+//     //         const cards = document.createElement('div');
+//     //         cards.setAttribute('class','card');
+//     //         cards.innerHTML=`
+//     //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+//     //         <p>${element.name}</p>
+//     //         `;
+//     //         container.append(cards)
+//     //      })
+//     // }else{
+//     //     icon.forEach((element)=>{
+//     //         container.innerHTML+='';
+//     //         const cards = document.createElement('div');
+//     //         cards.setAttribute('class','card');
+//     //         cards.innerHTML=`
+//     //         <i class="${element.family} ${element.prefix}${element.name}" style="color:${element.color}"></i>
+//     //         <p>${element.name}</p>
+//     //         `;
+//     //         container.append(cards)
+//     //      })
+//     // }
    
 }
 
 
 selezione.addEventListener('change', selezionaIcone);
-
-// filtro le icone per tipo 
 
 // 1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
 
